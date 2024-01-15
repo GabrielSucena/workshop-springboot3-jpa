cb020f9f2d03d0713b76898330f8b5c3936aab46
Gabriel Sucena <gabrielsucena04@gmail.com>
2024-01-14 22:10:25 -0300

    Order, Instant, ISO 8601

---
+++ src/main/java/com/gabrielsucena/course/services/OrderService.java
@@ -0,0 +1,27 @@
+package com.gabrielsucena.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gabrielsucena.course.entities.Order;
+import com.gabrielsucena.course.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll(){
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
